4df8da61e4523c38a7218a0d699ec7485e622126
ahmed Emam <a.emam@elithair.com>
2026-02-13 00:27:46 +0400

    Fix integration tests: Update CustomWebApplicationFactory namespace

--- tests/ServerMonitoring.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/ServerMonitoring.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
-using ServerMonitoring.API;
 
 namespace ServerMonitoring.IntegrationTests;
 
